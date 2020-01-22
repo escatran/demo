fe8ee01905fffd3c5443c4ef74c0a64f00963448
Tung Tran <tungdtran89@outlook.com>
2020-01-22 22:38:32 +0800

    initial commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
+        System.out.println("Receive request!");
         return "Hello world!";
     }
 }
